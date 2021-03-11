cd0438618b92aaaf4defe846e28a5abf44e4a186
Reinoptland <reinopland@gmail.com>
2021-03-11 08:56:19 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [score, setScore] = useState(0);
+
+  function plusOne() {
+    setScore(score + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ScoreBoard</h1>
+        <div>
+          <div>
+            <p class="counter counter--blue">Team blue: {score}</p>
+          </div>
+          <div class="buttons">
+            <button onClick={plusOne}>+ 1</button>
+            <button>- 1</button>
+            <button>+ 5</button>
+            <button>- 5</button>
+            <button>half score</button>
+            <button>reset</button>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p class="counter counter--red">Team red: 0</p>
+          </div>
+          <div class="buttons">
+            <button>+ 1</button>
+            <button>- 1</button>
+            <button>+ 5</button>
+            <button>- 5</button>
+            <button>half score</button>
+            <button>reset</button>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p class="counter counter--pink">Team pink: 0</p>
+          </div>
+          <div class="buttons">
+            <button>+ 1</button>
+            <button>- 1</button>
+            <button>+ 5</button>
+            <button>- 5</button>
+            <button>half score</button>
+            <button>reset</button>
+          </div>
+        </div>
       </header>
     </div>
   );
